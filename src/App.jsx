87cded6455cbc2751474368d93c731443eb936c4
--delete <--delete>
2024-01-17 23:38:40 +0600

    project finished

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+// import './App.css'
+
+import Footer from "./components/Footer/Footer"
+import HeroSection from "./components/HeroSection/HeroSection"
+import Navbar from "./components/Navbar/Navbar"
+import StudentInfo from "./components/StudentInfo/StudentInfo"
+
+function App() {
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <HeroSection></HeroSection>
+      <StudentInfo></StudentInfo>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
